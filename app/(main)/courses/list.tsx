87163d5033dcaadb9d0bf36dec7ea2839800b7ab
katsiev1997 <katsiev1997@gmail.com>
2024-03-30 01:20:34 +0300

    User Progress

--- app/(main)/courses/list.tsx
+++ app/(main)/courses/list.tsx
@@ -1,16 +1,31 @@
 "use client";
 
+import { upsertUserProgress } from "@/actions/user-progress";
 import { Card } from "@/app/(main)/courses/card";
-import { courses } from "@/db/schema";
+import { courses, userProgress } from "@/db/schema";
+import { useRouter } from "next/navigation";
 
 type Props = {
   courses: (typeof courses.$inferSelect)[];
-  activeCourseId: number;
+  activeCourseId?: typeof userProgress.$inferSelect.activeCourseId;
 };
 
-import React from "react";
+import React, { useTransition } from "react";
+import { toast } from "sonner";
 
 export const List = ({ courses, activeCourseId }: Props) => {
+  const router = useRouter();
+  const [pending, startTransition] = useTransition();
+
+  const onClick = (id: number) => {
+    if (pending) return;
+    if (id === activeCourseId) {
+      router.push("/learn");
+    }
+    startTransition(() => {
+      upsertUserProgress(id).catch(() => toast.error("Something went wrong"));
+    });
+  };
   return (
     <div className="pt-6 grid grid-cols-2 lg:grid-cols-[repeat(auto-fill,minmax(210px,1fr))] gap-4">
       {courses.map((course) => (
@@ -19,8 +34,8 @@ export const List = ({ courses, activeCourseId }: Props) => {
           id={course.id}
           title={course.title}
           imageSrc={course.imageSrc}
-          onClick={() => {}}
-          disabled={false}
+          onClick={onClick}
+          disabled={pending}
           active={course.id === activeCourseId}
         />
       ))}
