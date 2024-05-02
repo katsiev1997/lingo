07caa844714153f3e84f4151cccfa33cd86aea0d
katsiev1997 <katsiev1997@gmail.com>
2024-05-02 20:06:41 +0300

    Challenge Actions

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -96,16 +96,22 @@ const main = async () => {
       },
       {
         id: 2,
-        lessonId: 2, // Verbs
-        type: "SELECT",
+        lessonId: 1, // Verbs
+        type: "ASSIST",
         order: 2,
-        question: 'Which one of these is "the man"?',
+        question: "the man",
+      },
+      {
+        id: 3,
+        lessonId: 1, // Verbs
+        type: "SELECT",
+        order: 3,
+        question: 'Which one of these is "the robot"?',
       },
     ]);
 
     await db.insert(schema.challengeOptions).values([
       {
-        id: 1,
         challengeId: 1,
         imageSrc: "/man.svg",
         correct: true,
@@ -113,7 +119,6 @@ const main = async () => {
         audioSrc: "/es_man.mp3",
       },
       {
-        id: 2,
         challengeId: 1, // Which one of these is "the man"?
         imageSrc: "/woman.svg",
         correct: false,
@@ -121,7 +126,6 @@ const main = async () => {
         audioSrc: "/es_woman.mp3",
       },
       {
-        id: 3,
         challengeId: 1,
         imageSrc: "/robot.svg",
         correct: false,
@@ -130,6 +134,50 @@ const main = async () => {
       },
     ]);
 
+    await db.insert(schema.challengeOptions).values([
+      {
+        challengeId: 2,
+        correct: true,
+        text: "el hombre",
+        audioSrc: "/es_man.mp3",
+      },
+      {
+        challengeId: 2, // Which one of these is "the man"?
+        correct: false,
+        text: "la mujer",
+        audioSrc: "/es_woman.mp3",
+      },
+      {
+        challengeId: 2,
+        correct: false,
+        text: "el robot",
+        audioSrc: "/es_robot.mp3",
+      },
+    ]);
+
+    await db.insert(schema.challengeOptions).values([
+      {
+        challengeId: 3,
+        imageSrc: "/man.svg",
+        correct: false,
+        text: "el hombre",
+        audioSrc: "/es_man.mp3",
+      },
+      {
+        challengeId: 3, // Which one of these is "the man"?
+        imageSrc: "/woman.svg",
+        correct: false,
+        text: "la mujer",
+        audioSrc: "/es_woman.mp3",
+      },
+      {
+        challengeId: 3,
+        imageSrc: "/robot.svg",
+        correct: true,
+        text: "el robot",
+        audioSrc: "/es_robot.mp3",
+      },
+    ]);
     console.log("Seeding finished");
   } catch (error) {
     console.error(error);
